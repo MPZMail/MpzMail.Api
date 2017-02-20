4643e55edb5560fc89b7c37f25c812bc26ce737d
Mahmut <mah_sivasli@hotmail.com>
2017-02-20 16:57:07 +0100

    Customer
    Added method to retrieve customers "GetCustomers"

---
+++ MpzMail.Api/Model/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace MpzMail.Api.Model
+{
+    public class Customer
+    {
+        [XmlElement("fullName")]
+        public string FullName { get; set; }
+        [XmlElement("emailAddress")]
+        public string EmailAddress { get; set; }
+        [XmlElement("apiKey")]
+        public string ApiKey;
+    }
+}
---
+++ MpzMail.Api/Model/CustomerResult.cs
@@ -0,0 +1,20 @@
+﻿using MpzMail.Api.Model.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace MpzMail.Api.Model
+{
+    [XmlRoot("xml")]
+    public class CustomerResult : BaseResult
+    {
+        [XmlElement("userCnt")]
+        public int Count { get; set; }
+        [XmlArray("users")]
+        [XmlArrayItem("user")]
+        public List<Customer> Customers { get; set; }
+    }
+}
